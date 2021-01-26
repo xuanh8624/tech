6f4a58119aadf46eff17b818a2910b5dbebc3c5f
phucbmt <phucdlbmt@gmail.com>
2021-01-26 19:15:32 +0700

    Update L.sh

--- L.sh
+++ L.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-printf "Installing RDP Be Patience... " >&2
+printf "Installing RDP Be Patience... " $1 >&2
 {
 git clone https://github.com/phucbmt/tech.git &> /dev/null
 sudo chmod +x tech/m
